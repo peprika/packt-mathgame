02a99eaa7058143b97c007aab6d980f46013f713
Riku Pepponen <riku.pepponen@gmail.com>
2017-06-13 01:13:01 +0300

    Added the game screen

---
+++ app/src/main/java/com/packtpub/packt_mathgame/GameActivity.java
@@ -0,0 +1,18 @@
+package com.packtpub.packt_mathgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Riku Pepponen on 12.6.2017.
+ * (riku.pepponen@gmail.com)
+ */
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}

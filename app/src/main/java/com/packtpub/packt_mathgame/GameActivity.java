0521de505b434d6af4c8687412689609bba356f7
Riku Pepponen <riku.pepponen@gmail.com>
2017-06-13 14:31:13 +0300

    Variable declarations

--- app/src/main/java/com/packtpub/packt_mathgame/GameActivity.java
+++ app/src/main/java/com/packtpub/packt_mathgame/GameActivity.java
@@ -14,5 +14,12 @@ public class GameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        int partA = 9;
+        int partB = 9;
+        int correctAnswer = partA * partB;
+        int wrongAnswer1 = correctAnswer - 1;
+        int wrongAnswer2 = correctAnswer + 1;
+        
     }
 }

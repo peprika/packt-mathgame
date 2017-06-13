33b0aeeaedfe4a873ee1047328d0de41da9f5331
Riku Pepponen <riku.pepponen@gmail.com>
2017-06-13 14:39:58 +0300

    UI element declarations

--- app/src/main/java/com/packtpub/packt_mathgame/GameActivity.java
+++ app/src/main/java/com/packtpub/packt_mathgame/GameActivity.java
@@ -2,6 +2,10 @@ package com.packtpub.packt_mathgame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Riku Pepponen on 12.6.2017.
@@ -15,11 +19,18 @@ public class GameActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        // Variable declarations
         int partA = 9;
         int partB = 9;
         int correctAnswer = partA * partB;
         int wrongAnswer1 = correctAnswer - 1;
         int wrongAnswer2 = correctAnswer + 1;
-        
+
+        // UI element declarations
+        TextView textObjectPartA = (TextView)findViewById(R.id.textPartA);
+        TextView textObjectPartB = (TextView)findViewById(R.id.textPartB);
+        Button buttonObjectChoice1 = (Button)findViewById(R.id.buttonChoice1);
+        Button buttonObjectChoice2 = (Button)findViewById(R.id.buttonChoice2);
+        Button buttonObjectChoice3 = (Button)findViewById(R.id.buttonChoice3);
     }
 }

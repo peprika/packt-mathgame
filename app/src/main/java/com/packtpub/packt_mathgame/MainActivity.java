72969da1e0efab39132419e9900968592e59e802
Riku Pepponen <riku.pepponen@gmail.com>
2017-06-13 14:28:41 +0300

    Declared activity in the manifest

--- app/src/main/java/com/packtpub/packt_mathgame/MainActivity.java
+++ app/src/main/java/com/packtpub/packt_mathgame/MainActivity.java
@@ -1,13 +1,27 @@
 package com.packtpub.packt_mathgame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newGameButton = (Button)findViewById(R.id.new_game_button);
+        newGameButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        i = new Intent(this, GameActivity.class);
+        startActivity(i);
+    }
+
 }
